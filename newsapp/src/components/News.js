6742365e072912e036ccdbcd904852fe1e87e2a1
nitingoyt <Nitingoyat99965@gmail.com>
2024-11-25 22:49:46 +0530

    adding infinite scrool to newsAPP

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,109 +1,103 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  //   document.title = `${props.category}`;
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-
-  constructor() {
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-      totalResults: 0,
-    };
-  }
-
-  async updateNews(page) {
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e7a39ba7b8494599bd4dbff03d36a8ee&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const updateNews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=e7a39ba7b8494599bd4dbff03d36a8ee&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parseData = await data.json();
-    console.log(parseData);
-    this.setState({
-      articles: parseData.articles, 
-      totalResults: parseData.totalResults,
-      loading: false,
-      page: page
-    });
-  }
+    setArticles(parseData.articles);
+    setTotalResults(parseData.totalResults);
+    setLoading(false);
 
-  async componentDidMount() {
-     await this.updateNews(this.state.page)
-  }
-
-  handlePrevClick = async () => {
-
-    const newPage = this.state.page -1 ;
-    await this.updateNews(newPage);
+    props.setProgress(100);
   };
 
-  handleNextClick = async () => {
-    const newPage = this.state.page + 1 ;
-    await this.updateNews(newPage);
+  useEffect(() => {
+    updateNews();
+  }, []);
+
+  const handlePrevClick = async () => {
+    setPage(page-1);
+    updateNews();
   };
 
+  const handleNextClick = async () => {
+    setPage(page+1);
+    updateNews();
 
-  render() {
+    const capitalizeFirstletter = (string) => {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    const fetchMoreData = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=e7a39ba7b8494599bd4dbff03d36a8ee&page=${page+1}&pageSize=${props.pageSize}`;
+      setPage(page+1);
+      let data = await fetch(url);
+      let parseData = await data.json();
+      setArticles(articles.concat(parseData.articles));
+      setTotalResults(parseData.totalResults);
+    };
+
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center">
-          NewsKabootr - {this.props.category} Headlines
+          NewsKabootr - {capitalizeFirstletter(props.category)} Headlines
         </h1>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                  />
-                </div>
-              );
-            })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePrevClick}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              this.state.page  >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next &rarr;
-          </button>
-        </div>
-      </div>
+        {/*loading && <Spinner />}*/}
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </InfiniteScroll>
+      </>
     );
-  }
-}
 
+};
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+ 
 export default News;
